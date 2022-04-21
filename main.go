edfa2c86d3f7e6a43a01bad3a636feb70b2d6217
phdevbr <pedroheringer2020@gmail.com>
2022-04-21 14:26:07 -0300

    Created models folder

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/phdevbr/go-rest-api/models"
 	"github.com/phdevbr/go-rest-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome1", Historia: "Historia1"},
+		{Nome: "Nome2", Historia: "Historia2"},
+	}
+
 	fmt.Println("Iniciando o servidor REST com Go...")
 	routes.HandleRequest()
 }
