2805a826eda6df820ef5e0780f4e31bbe8b1f751
phdevbr <pedroheringer2020@gmail.com>
2022-04-22 13:00:24 -0300

    Added Create Personality Route

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/phdevbr/go-rest-api/database"
 	"github.com/phdevbr/go-rest-api/models"
 )
 
@@ -15,16 +15,22 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
 
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novapersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novapersonalidade)
+	database.DB.Create(&novapersonalidade)
+	json.NewEncoder(w).Encode(novapersonalidade)
 }
